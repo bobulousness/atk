94dcb578f4e2413ee33a94e3fff0b103e46c7296
Josh Bumgarner <bobulousness@gmail.com>
2015-11-07 12:01:53 -0500

    save

---
+++ public/js/boot.js
@@ -0,0 +1,18 @@
+//state: flappy.boot
+
+'use strict';
+
+var flappy = { };
+var flappy.boot = function () {};
+
+flappy.boot.prototype = {
+  init: function () {
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+    this.game.stage.backgroundColor = "magenta"
+  },
+};
